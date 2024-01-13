70b3799a30c275502ebaab5fe6855e6873fb3a0d
DevSpice <60044431+DevSpice@users.noreply.github.com>
2024-01-13 01:27:02 -0600

    Update minimum-number-of-steps-to-make-two-strings-anagram.py

--- minimum-number-of-steps-to-make-two-strings-anagram.py
+++ minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,3 +1,16 @@
+'''
+1347
+
+You are given two strings of the same length s and t. In one step you 
+can choose any character of t and replace it with another character.
+
+Return the minimum number of steps to make t an anagram of s.
+
+'''
+
+
+
+
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
         for x in s:
